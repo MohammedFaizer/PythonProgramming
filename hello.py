95e2bf4cf3714a5871ab55b578c96e1f53a70074
MohammedFaizer <zmd5zer@gmail.com>
2022-11-11 14:24:47 +0530

    seccond

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 print('hellod world')
-a=input('enter the number to be searched:')
+a=int(input('enter the number to be searched:'))
 print('the number you typed is ',a)
 fruits = ['apple', 'banana', 'cherry']
 for x in fruits:
   print(x)
+print(fruits[a])
