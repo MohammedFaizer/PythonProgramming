84805e65353c0c6fd066da4684eec9db48125572
MohammedFaizer <zmd5zer@gmail.com>
2022-11-25 21:40:57 +0530

    finished

---
+++ e.py
@@ -0,0 +1,7 @@
+import datetime  as dt
+today_date=input("Enter the todays date:\n")
+today_date=dt.datetime.strptime(today_date,'%d/%m/%Y').date()
+birth_date=input('Enter your Birth date:\n')
+birth_date=dt.datetime.strptime(birth_date,'%d/%m/%Y').date()
+da=today_date-birth_date
+print(da.days//365)
