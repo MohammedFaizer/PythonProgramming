686a4af6d73d8e6057d7ea11f0bc7de533f6632d
MohammedFaizer <zmd5zer@gmail.com>
2022-11-27 00:25:00 +0530

    finished

---
+++ i4.py
@@ -0,0 +1,8 @@
+for i in range(0,5):
+    for k in range(0,i+1):
+            print('*',end='')
+    print("")
+for i in range(1,5):
+    for k in range(i,5):
+            print('*',end='')
+    print("")    
