7329adfc22be90e89314989b71a9fdde9f115358
MohammedFaizer <zmd5zer@gmail.com>
2022-11-27 00:03:29 +0530

    finished

---
+++ i2.py
@@ -0,0 +1,15 @@
+print("hello")
+for i in range(1,6):
+    for j in range(1,6):
+        if(i==1 or j==1 or j==5 or i==5):
+            if(i==j):
+                print("$",end='\t')
+            else:
+                print("*",end='\t')
+        else:
+            if(i==j):
+                print("$",end='\t')
+            else:
+                print(" ",end='\t') 
+               
+    print('\n')    
